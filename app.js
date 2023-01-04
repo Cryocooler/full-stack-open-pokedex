b64375bf9817c2ba617d533261d36268bbc06e70
Sahin Dursun <sahin.dursun@relexsolutions.com>
2023-01-04 12:16:35 +0200

    lint fixes2

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
+const express = require('express')
+const app = express()
+require('dotenv').config()
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 3007
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`server started on ${PORT}`);
-});
+  console.log(`server started on ${PORT}`)
+})
